4de87617c1d19ed3ae9d90e04d6c8502894bf592
刘洋 <liuyang@biucloud.com>
2017-10-24 18:52:27 +0800

    优化界面及功能

--- FindReferences.cs
+++ FindReferences.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -31,45 +31,59 @@ public class FindReferences : EditorWindow
     private string[] _files;
     private readonly List<FileData> _fileDatas = new List<FileData>();
     private readonly List<string> _extensions = new List<string> { ".prefab", ".unity", ".mat", ".asset" };
+    private string _selectedFold = "";
+    private bool _isGlobal = true;
+    private Vector2 _scrollPos;
 
     private void OnGUI()
     {
         GUILayout.Space(10);
-        if (GUILayout.Button("刷新数据"))
+
+        _isGlobal = EditorGUILayout.ToggleLeft("全局查找", _isGlobal);
+        GUI.enabled = !_isGlobal;
+
+        GUILayout.Space(5);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(20);
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("选择目录"))
+            _selectedFold = EditorUtility.OpenFolderPanel("选择查找目录", "", "");
+        GUILayout.Label("当前查询目录：" + _selectedFold);
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+
+        GUI.enabled = true;
+
+        GUILayout.Space(10);
+        if (GUILayout.Button(new GUIContent("初始化数据", "当资源改变需要重新初始化数据")))
         {
-            UpdateFiles();
+            string fold = _isGlobal ? Application.dataPath : _selectedFold;
+            Debug.Log(fold);
+            if (string.IsNullOrEmpty(fold))
+                Debug.LogError("抱歉，请先选择目录！");
+            else
+                UpdateFiles(fold);
         }
 
         _currObject = Selection.activeObject;
+
         GUILayout.Space(10);
         _currObject = EditorGUILayout.ObjectField("当前选中：", _currObject, typeof(Object), false);
+
         GUILayout.Space(10);
-        _foundObject = EditorGUILayout.ObjectField("被查找：", _foundObject, typeof(Object), false);
-        GUILayout.Space(20);
-
-        if (_resultObjects != null)
-        {
-            GUILayout.Label("查找结果：");
-            foreach (var resultObject in _resultObjects)
-            {
-                EditorGUILayout.ObjectField("", resultObject, typeof(Object), false);
-            }
-        }
-
         if (GUILayout.Button("查找当前选中资源"))
         {
             if (_fileDatas.Count == 0)
             {
-                Debug.LogError("抱歉，请刷新数据后使用！");
+                Debug.LogError("抱歉，第一次使用请初始化数据！");
                 _foundObject = null;
                 _resultObjects = null;
             }
             else
             {
                 if (_currObject == null)
-                {
                     Debug.LogError("抱歉，请选中后使用！");
-                }
                 else
                 {
                     _foundObject = _currObject;
@@ -78,11 +92,31 @@ public class FindReferences : EditorWindow
                 }
             }
         }
+
+        GUILayout.Space(30);
+        _foundObject = EditorGUILayout.ObjectField("被查找：", _foundObject, typeof(Object), false);
+        GUILayout.Space(30);
+
+        if (_resultObjects != null)
+        {
+            GUILayout.Label("查找结果：");
+            if (_resultObjects.Length > 0)
+            {
+                _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.Height(300));
+
+                foreach (var resultObject in _resultObjects)
+                    EditorGUILayout.ObjectField("", resultObject, typeof(Object), false);
+
+                EditorGUILayout.EndScrollView();
+            }
+            else
+                GUILayout.Label("未找到该资源的引用：" + _foundObject.name);
+        }
     }
 
-    private void UpdateFiles()
+    private void UpdateFiles(string path)
     {
-        var files = Directory.GetFiles(Application.dataPath, "*.*", SearchOption.AllDirectories)
+        var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
             .Where(x => _extensions.Contains(Path.GetExtension(x).ToLower())).ToArray();
 
         int idx = 0;
@@ -91,7 +125,7 @@ public class FindReferences : EditorWindow
         EditorApplication.update = () =>
         {
             string file = files[idx];
-            EditorUtility.DisplayProgressBar("匹配资源中", file, idx / (float)files.Length);
+            EditorUtility.DisplayProgressBar("初始化数据中", file, idx / (float)files.Length);
 
             _fileDatas.Add(new FileData(file, File.ReadAllText(file)));
 
@@ -103,7 +137,7 @@ public class FindReferences : EditorWindow
             EditorUtility.ClearProgressBar();
             idx = 0;
             EditorApplication.update = null;
-            Debug.Log("刷新完成！");
+            Debug.Log("初始化完成！");
         };
     }
 
